d5fca6a435d7886f1551f31a09e29e4d35445bd7
Matheus Henrique Bertuci <mateusbertuci@hotmail.com>
2023-11-20 16:58:28 -0300

    Update Chat_GIS.py

--- Chat_GIS.py
+++ Chat_GIS.py
@@ -4,7 +4,7 @@ import os
 import time
 
 def setup_openai():
-    api_key = os.getenv("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")
+    api_key = os.getenv("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")
     openai.api_key = api_key
 
 def chat_with_openai_with_throttling(prompt, model="text-davinci-003", max_tokens=300, temperature=0.8, stop_sequences=None):
