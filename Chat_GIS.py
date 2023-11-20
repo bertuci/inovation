e7edba1afe1a6d8e9304ce948ac52de770ae7872
Matheus Henrique Bertuci <mateusbertuci@hotmail.com>
2023-11-20 11:00:11 -0300

    Update Chat_GIS.py

--- Chat_GIS.py
+++ Chat_GIS.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import openai
-
+import time
 
 def setup_openai():
-    api_key = "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"
+    api_key = "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"
     openai.api_key = api_key
 
-
-def chat_with_openai(prompt, model="text-davinci-003", max_tokens=300, temperature=0.8, stop_sequences=None):
+def chat_with_openai_with_throttling(prompt, model="text-davinci-003", max_tokens=300, temperature=0.8, stop_sequences=None):
+    time.sleep(2)  # Aguarde 2 segundos entre as chamadas
     response = openai.Completion.create(
         engine=model,
         prompt=prompt,
@@ -17,13 +17,12 @@ def chat_with_openai(prompt, model="text-davinci-003", max_tokens=300, temperatu
     )
     return response.choices[0].text.strip()
 
-
 def main():
     setup_openai()
     st.title("Bem-vindo(a) ao Chat GIS!")
 
     st.write("Digite sua pergunta abaixo:")
-    user_input = st.text_input("", "")
+    user_input = st.text_area("")
 
     if st.button("Enviar"):
         st.subheader("Sua pergunta:")
@@ -32,10 +31,9 @@ def main():
         esri_products = ["ArcGIS PRO", "ArcGIS Online", "ArcGIS Enterprise"]
         esri_prompt = f"Produtos ESRI: {', '.join(esri_products)}\n\nVocê: {user_input}\nChatbot:"
 
-        response = chat_with_openai(esri_prompt, max_tokens=2048)  # Ajuste max_tokens para um número maior
+        response = chat_with_openai_with_throttling(esri_prompt, max_tokens=2048)  # Ajuste max_tokens para um número maior
         st.subheader("Resposta:")
         st.write(response)
-
 
 if __name__ == "__main__":
     main()
